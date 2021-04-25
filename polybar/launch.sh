cfe04d1a7cd747cba0d1b6cb05332534c37da04f
tiagovla <tiagovla@gmail.com>
2021-04-25 19:34:55 -0300

    Update polybar modules and colors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+killall -q polybar && sleep 2
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log 
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+echo "---" | tee -a /tmp/polybar1.log
+
+polybar tokyodark 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
