406f2fced5c523557c826487ad5dd960950a2c88
tiagovla <tiagovla@gmail.com>
2021-04-30 02:21:21 -0300

    Update install script

--- scripts/install/install_neovim.sh
+++ scripts/install/install_neovim.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if uname -a | grep -q Ubuntu; then
+	echo "Installing dependencies on ubuntu..."
+	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+elif [ -f "/etc/arch-release" ]; then
+	echo "Installing dependencies on arch..."
+	sudo pacman -S base-devel cmake unzip ninja tree-sitter
+fi
+
 TMP_DIR=$(mktemp -d)
 if [ ! -e "$TMP_DIR" ]; then
 	echo >&2 "Failed to create temp directory."
@@ -10,14 +18,6 @@ trap 'rm -rf "$TMP_DIR"' EXIT
 echo "Created temporary directory $TMP_DIR."
 
 NEOVIM_TMP_DIR="$TMP_DIR/neovim"
-
-if uname -a | grep -q Ubuntu; then
-	echo "Installing dependencies on ubuntu..."
-	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
-elif [ -f "/etc/arch-release" ]; then
-	echo "Installing dependencies on arch..."
-	sudo pacman -S base-devel cmake unzip ninja tree-sitter
-fi
 
 git clone https://github.com/neovim/neovim.git "$NEOVIM_TMP_DIR"
 cd "$NEOVIM_TMP_DIR" || exit 1
