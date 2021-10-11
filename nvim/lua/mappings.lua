93b482f6352b324ef056c819da5be8188973a394
tiagovla <tiagovla@gmail.com>
2021-10-10 21:47:59 -0300

    Update: new tab hotkeys

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,18 +9,23 @@ end
 
 function M.movements()
     local movements_mappings = {
-        {"n", "<C-J>", "<C-W><C-J>"}, {"n", "<C-K>", "<C-W><C-K>"}, {"n", "<C-L>", "<C-W><C-L>"},
-        {"n", "<c-H>", "<C-W><C-H>"}, {"n", "<c-N>", ":bnext<cr>", {"noremap", "silent"}},
-        {"n", "<c-P>", ":bprev<cr>", {"noremap", "silent"}},
+        {"n", "<C-J>", "<C-W><C-J>"}, {"n", "<C-K>", "<C-W><C-K>"},
+        {"n", "<C-L>", "<C-W><C-L>"}, {"n", "<c-H>", "<C-W><C-H>"},
+        {"n", "<c-N>", ":bnext<cr>", {"noremap", "silent"}},
+        {"n", "<c-P>", ":bprev<cr>", {"noremap", "silent"}}, {"n", "H", "gt"},
+        {"n", "L", "gT"}
     }
     ezmap.map(movements_mappings)
 end
 
-function M.general()
-end
+function M.general() end
 
 function M.plugins()
+    local plugins_mappings = {
+        {"n", "<space>gd", ":DogeGenerate <cr>"},
+        {"n", "<space>gr", ":lua require'utils.lsp'.rename() <cr>"}
+    }
+    require("ezmap").map(plugins_mappings, {"noremap", "silent"})
 end
 
 return M
-
