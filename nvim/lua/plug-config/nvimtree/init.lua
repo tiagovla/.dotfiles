20a51275f98b27c7592ac73702564b70b62450c6
tiagovla <tiagovla@gmail.com>
2021-10-10 21:44:03 -0300

    Update: nvimtree settings after breaking update

--- nvim/lua/plug-config/nvimtree/init.lua
+++ nvim/lua/plug-config/nvimtree/init.lua
@@ -1,12 +1,11 @@
 pcall(require, "plug-config.nvimtree.mappings")
 
 local config = {
-    cmd = {"NvimTreeOpen", "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeRefresh"},
+    -- cmd = {
+    --     "NvimTreeOpen", "NvimTreeToggle", "NvimTreeFindFile", "NvimTreeRefresh"
+    -- },
     requires = {"kyazdani42/nvim-web-devicons"},
-    config = function()
-
-        require "plug-config.nvimtree.settings"
-    end,
+    config = function() require "plug-config.nvimtree.settings" end
 }
 
 return config
