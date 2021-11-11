a775cb3ba567df663761ab27e9b2fc260a4570ab
tiagovla <tiagovla@gmail.com>
2021-11-10 23:35:55 -0300

    perf(dap): simplify settings

--- neovim/.config/nvim/lua/plugins/config/dap/settings.lua
+++ neovim/.config/nvim/lua/plugins/config/dap/settings.lua
@@ -7,16 +7,9 @@ dap.adapters.python = {
     args = { "-m", "debugpy.adapter" },
 }
 
-local is_windows = function()
-    return vim.loop.os_uname().sysname:find("Windows", 1, true) and true
-end
-
 local get_python_path = function()
     local venv_path = os.getenv "VIRTUAL_ENV"
     if venv_path then
-        if is_windows() then
-            return venv_path .. "\\Scripts\\python.exe"
-        end
         return venv_path .. "/bin/python"
     end
     return nil
@@ -31,6 +24,10 @@ dap.configurations.python = {
         program = "${file}",
         pythonPath = function()
             local cwd = vim.fn.getcwd()
+            if vim.fn.glob(cwd .. "/poetry.lock") then
+                local venv = vim.fn.trim(vim.fn.system "poetry env info -p")
+                return venv .. "/bin" .. "/python"
+            end
             if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
                 return cwd .. "/venv/bin/python"
             elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
