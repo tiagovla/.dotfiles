90af476e63939ff51a188557bf6dc9f21354007d
Tiago Vilela <tiagovla@gmail.com>
2022-03-01 01:48:13 -0300

    fix: local scope and local cmd

--- neovim/.config/nvim/lua/plugins/config/scope.lua
+++ neovim/.config/nvim/lua/plugins/config/scope.lua
@@ -1,21 +1,12 @@
-local session = setmetatable({}, {
+local M = {}
+
+M.session = setmetatable({}, {
     __index = function(table, key)
         local rtn = {}
         rawset(table, key, rtn)
         return rtn
     end,
 })
-
-vim.api.nvim_exec(
-    [[
-      augroup ScopeAU
-        autocmd!
-        autocmd BufEnter * lua require("plugins.config.scope").on_buf_open()
-        autocmd BufDelete * lua require("plugins.config.scope").on_buf_close()
-      augroup end
-    ]],
-    false
-)
 
 local function get_buf_tab()
     local buf = vim.api.nvim_get_current_buf()
@@ -26,27 +17,54 @@ local function get_buf_tab()
     return {}
 end
 
-local M = {}
-
 function M.on_buf_open()
     local tab, buf = unpack(get_buf_tab())
-    if tab == nil then
-        return
+    if tab ~= nil then
+        M.session[tab][buf] = buf
     end
-    session[tab][buf] = buf
 end
 
-function M.on_buf_close()
+function M.on_buf_delete()
     local tab, buf = unpack(get_buf_tab())
-    if tab == nil then
-        return
+    if tab ~= nil then
+        M.session[tab][buf] = nil
     end
-    session[tab][buf] = nil
+end
+
+function M.on_tab_close()
+    local tab_list = vim.api.nvim_list_tabpages()
+    for _, tab in pairs(vim.tbl_keys(M.session)) do
+        if not vim.tbl_contains(tab_list, tab) then
+            M.session[tab] = nil
+        end
+    end
+end
+
+function M.close_all_bufs_but_this()
+    local tab, buf = unpack(get_buf_tab())
+    if tab ~= nil then
+        M.session[tab][buf] = nil
+    end
 end
 
 function M.show_bufs()
     local tab = vim.api.nvim_get_current_tabpage()
-    return vim.tbl_values(session[tab])
+    return vim.tbl_values(M.session[tab])
 end
+
+local function print_summary()
+    for tab, buf_item in pairs(M.session) do
+        for buf, _ in pairs(buf_item) do
+            local name = vim.api.nvim_buf_get_name(buf)
+            print(tab .. " " .. buf .. " " .. name)
+        end
+    end
+end
+
+vim.api.nvim_create_augroup("ScopeAU", {})
+vim.api.nvim_create_autocmd("BufEnter", { group = "ScopeAU", callback = M.on_buf_open })
+vim.api.nvim_create_autocmd("BufDelete", { group = "ScopeAU", callback = M.on_buf_delete })
+vim.api.nvim_create_autocmd("TabClosed", { group = "ScopeAU", callback = M.on_tab_close })
+vim.api.nvim_add_user_command("ScopeList", print_summary, {})
 
 return M
--- neovim/.config/nvim/lua/tiagovla/autocmds.lua
+++ neovim/.config/nvim/lua/tiagovla/autocmds.lua
@@ -1,40 +1,43 @@
-local M = {}
 local history = {}
+local last_exit = 1
+local M = {}
 
-vim.api.nvim_exec(
-    [[
-      augroup TrackCWD
-        autocmd!
-        autocmd BufEnter * lua require"tiagovla.autocmds".on_enter()
-        autocmd BufLeave * lua require"tiagovla.autocmds".on_leave()
-      augroup end
-    ]],
-    false
-)
+local function check_valid_buf(buf)
+    return vim.api.nvim_buf_get_option(buf, "buflisted") and buf ~= last_exit
+end
 
-function M.on_enter()
+local function on_enter()
     local buf = vim.api.nvim_get_current_buf()
-
-    if not vim.api.nvim_buf_get_option(buf, "buflisted") then
+    if not check_valid_buf(buf) then
         return
     end
 
     local past_path = history[buf]
     if past_path then
-        vim.fn.execute("cd " .. past_path, "silent")
+        vim.fn.execute("cd " .. past_path)
         require("nvim-tree").change_dir(past_path) --update nvimtree
     end
 end
 
-function M.on_leave()
+local function on_leave()
     local buf = vim.api.nvim_get_current_buf()
-
-    if not vim.api.nvim_buf_get_option(buf, "buflisted") then
+    if not check_valid_buf(buf) then
         return
     end
-
-    local path = vim.fn.getcwd(-1, -1)
-    history[buf] = path
+    last_exit = buf
+    history[buf] = vim.fn.getcwd(-1, -1)
 end
+
+local function print_summary()
+    for k, v in pairs(history) do
+        local name = vim.api.nvim_buf_get_name(k)
+        print(name .. " " .. v)
+    end
+end
+
+vim.api.nvim_create_augroup("TrackCWD", {})
+vim.api.nvim_create_autocmd("BufEnter", { group = "TrackCWD", callback = on_enter })
+vim.api.nvim_create_autocmd("BufLeave", { group = "TrackCWD", callback = on_leave })
+vim.api.nvim_add_user_command("PWDList", print_summary, {})
 
 return M
