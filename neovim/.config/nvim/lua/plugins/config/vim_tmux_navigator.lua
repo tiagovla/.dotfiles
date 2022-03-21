bf2adaeb0b11c2a261454a1751dcddbb8fcbe00b
Tiago Vilela <tiagovla@gmail.com>
2022-03-21 20:30:44 -0300

    feat: enable tmux on leave

--- neovim/.config/nvim/lua/plugins/config/vim_tmux_navigator.lua
+++ neovim/.config/nvim/lua/plugins/config/vim_tmux_navigator.lua
@@ -1,6 +1,7 @@
 return {
     opt = true,
     setup = function()
+        vim.api.nvim_create_autocmd("VimLeave", { command = "silent !tmux set status on" })
         vim.defer_fn(function()
             require("packer").loader "vim-tmux-navigator"
         end, 50)
