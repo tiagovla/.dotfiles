24c966984a3e2b62f6426784dac1ddf4f37cf21b
Tiago Vilela <tiagovla@gmail.com>
2022-03-21 20:30:01 -0300

    fix: do not use treesitter for big files

--- neovim/.config/nvim/lua/plugins/config/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/config/treesitter.lua
@@ -2,7 +2,17 @@ local function config()
     local treesitter = require "nvim-treesitter.configs"
     treesitter.setup {
         ensure_installed = "maintained",
-        highlight = { enable = true, additional_vim_regex_highlighting = false, use_languagetree = true },
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+            use_languagetree = false,
+            disable = function(lang, bufnr)
+                print(bufnr)
+                local buf_name = vim.api.nvim_buf_get_name(bufnr)
+                local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+                return file_size > 256 * 1024
+            end,
+        },
     }
 end
 
