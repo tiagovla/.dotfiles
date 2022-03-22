06099a6b9454c3b5c9c3e24d21238cd49e70e85d
Tiago Vilela <tiagovla@gmail.com>
2022-03-22 19:16:42 -0300

    fix: remove print

--- neovim/.config/nvim/lua/plugins/config/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/config/treesitter.lua
@@ -7,7 +7,6 @@ local function config()
             additional_vim_regex_highlighting = false,
             use_languagetree = false,
             disable = function(lang, bufnr)
-                print(bufnr)
                 local buf_name = vim.api.nvim_buf_get_name(bufnr)
                 local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
                 return file_size > 256 * 1024
