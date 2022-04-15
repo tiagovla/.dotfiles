2f5561080cd73dbd283a0c01970b7a1c392ea705
Tiago Vilela <tiagovla@gmail.com>
2022-04-15 17:29:37 -0300

    feat: update mappings

--- neovim/.config/nvim/lua/plugins/config/cmp/mappings.lua
+++ neovim/.config/nvim/lua/plugins/config/cmp/mappings.lua
@@ -1,4 +1,22 @@
 local cmp = require "cmp"
+local ls = require "luasnip"
+
+vim.keymap.set("i", "<c-u>", require "luasnip.extras.select_choice")
+vim.keymap.set("i", "<c-l>", function()
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
+end)
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+    if ls.jumpable(-1) then
+        ls.jump(-1)
+    end
+end, { silent = true })
 
 local mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
