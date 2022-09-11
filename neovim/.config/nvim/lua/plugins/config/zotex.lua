d1a2b8eb9b2d20e5f08d05255fa38e8d4d41282f
tiagovla <tiagovla@gmail.com>
2022-09-11 03:09:01 -0300

    fix: update requirement

--- neovim/.config/nvim/lua/plugins/config/zotex.lua
+++ neovim/.config/nvim/lua/plugins/config/zotex.lua
@@ -2,5 +2,5 @@ return {
     config = function()
         require("zotex").setup {}
     end,
-    requires = { "kkharji/sqlite.lua", branch = "feat/support_sqlite_open_v2" },
+    requires = { "kkharji/sqlite.lua" },
 }
