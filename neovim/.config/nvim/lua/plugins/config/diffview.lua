bf7f7b35e16249f7fa2b79243b9082c039fc0bf3
tiagovla <tiagovla@gmail.com>
2022-09-11 03:04:15 -0300

    chore: update diffview settings

--- neovim/.config/nvim/lua/plugins/config/diffview.lua
+++ neovim/.config/nvim/lua/plugins/config/diffview.lua
@@ -1,8 +1,23 @@
+local function setup()
+    vim.keymap.set("n", "<leader>D", ":DiffviewToggle<cr>", { silent = true })
+end
+
 local function config()
-    local cb = require("diffview.config").diffview_callback
+    local actions = require "diffview.actions"
+
+    vim.api.nvim_create_user_command("DiffviewToggle", function(e)
+        local view = require("diffview.lib").get_current_view()
+        if view then
+            vim.cmd "DiffviewClose"
+        else
+            vim.cmd("DiffviewOpen " .. e.args)
+        end
+    end, { nargs = "*" })
+
     require("diffview").setup {
         diff_binaries = false,
         enhanced_diff_hl = false,
+        git_cmd = { "git" },
         use_icons = true,
         icons = {
             folder_closed = "",
@@ -11,88 +26,144 @@ local function config()
         signs = {
             fold_closed = "",
             fold_open = "",
+            done = "✓",
+        },
+        view = {
+            default = {
+                layout = "diff2_horizontal",
+            },
+            merge_tool = {
+                layout = "diff3_horizontal",
+                disable_diagnostics = true,
+            },
+            file_history = {
+
+                layout = "diff2_horizontal",
+            },
         },
         file_panel = {
-            position = "left",
-            width = 35,
-            height = 10,
             listing_style = "tree",
             tree_options = {
                 flatten_dirs = true,
-                folder_statuses = "always",
+                folder_statuses = "only_folded",
+            },
+            win_config = {
+                position = "left",
+                width = 35,
             },
         },
         file_history_panel = {
-            position = "bottom",
-            width = 35,
-            height = 16,
             log_options = {
-                max_count = 256,
-                follow = false,
-                all = false,
-                merges = false,
-                no_merges = false,
-                reverse = false,
+                single_file = {
+                    diff_merges = "combined",
+                },
+                multi_file = {
+                    diff_merges = "first-parent",
+                },
+            },
+            win_config = {
+                position = "bottom",
+                height = 16,
             },
         },
-        key_bindings = {
-            disable_defaults = false,
+        commit_log_panel = {
+            win_config = {},
+        },
+        default_args = {
+            DiffviewOpen = {},
+            DiffviewFileHistory = {},
+        },
+        hooks = {},
+        keymaps = {
+            disable_defaults = true,
             view = {
-                ["<tab>"] = cb "select_next_entry",
-                ["<s-tab>"] = cb "select_prev_entry",
-                ["gf"] = cb "goto_file",
-                ["<C-w><C-f>"] = cb "goto_file_split",
-                ["<C-w>gf"] = cb "goto_file_tab",
-                ["<leader>e"] = cb "focus_files",
-                ["<leader>b"] = cb "toggle_files",
+                ["<tab>"] = actions.select_next_entry,
+                ["<s-tab>"] = actions.select_prev_entry,
+                ["gf"] = actions.goto_file,
+                ["<C-w><C-f>"] = actions.goto_file_split,
+                ["<C-w>gf"] = actions.goto_file_tab,
+                ["<leader>e"] = actions.focus_files,
+                ["<leader>b"] = actions.toggle_files,
+                ["g<C-x>"] = actions.cycle_layout,
+                ["[x"] = actions.prev_conflict,
+                ["]x"] = actions.next_conflict,
+                ["<leader>co"] = actions.conflict_choose "ours",
+                ["<leader>ct"] = actions.conflict_choose "theirs",
+                ["<leader>cb"] = actions.conflict_choose "base",
+                ["<leader>ca"] = actions.conflict_choose "all",
+                ["dx"] = actions.conflict_choose "none",
+            },
+            diff1 = {},
+            diff2 = {},
+            diff3 = {
+
+                { { "n", "x" }, "2do", actions.diffget "ours" },
+                { { "n", "x" }, "3do", actions.diffget "theirs" },
+            },
+            diff4 = {
+
+                { { "n", "x" }, "1do", actions.diffget "base" },
+                { { "n", "x" }, "2do", actions.diffget "ours" },
+                { { "n", "x" }, "3do", actions.diffget "theirs" },
             },
             file_panel = {
-                ["j"] = cb "next_entry",
-                ["<down>"] = cb "next_entry",
-                ["k"] = cb "prev_entry",
-                ["<up>"] = cb "prev_entry",
-                ["<cr>"] = cb "select_entry",
-                ["o"] = cb "select_entry",
-                ["<2-LeftMouse>"] = cb "select_entry",
-                ["-"] = cb "toggle_stage_entry",
-                ["S"] = cb "stage_all",
-                ["U"] = cb "unstage_all",
-                ["X"] = cb "restore_entry",
-                ["R"] = cb "refresh_files",
-                ["<tab>"] = cb "select_next_entry",
-                ["<s-tab>"] = cb "select_prev_entry",
-                ["gf"] = cb "goto_file",
-                ["<C-w><C-f>"] = cb "goto_file_split",
-                ["<C-w>gf"] = cb "goto_file_tab",
-                ["i"] = cb "listing_style",
-                ["f"] = cb "toggle_flatten_dirs",
-                ["<leader>e"] = cb "focus_files",
-                ["<leader>b"] = cb "toggle_files",
+                ["j"] = actions.next_entry,
+                ["<down>"] = actions.next_entry,
+                ["k"] = actions.prev_entry,
+                ["<up>"] = actions.prev_entry,
+                ["<cr>"] = actions.select_entry,
+                ["o"] = actions.select_entry,
+                ["<2-LeftMouse>"] = actions.select_entry,
+                ["-"] = actions.toggle_stage_entry,
+                ["S"] = actions.stage_all,
+                ["U"] = actions.unstage_all,
+                ["X"] = actions.restore_entry,
+                ["R"] = actions.refresh_files,
+                ["l"] = actions.open_commit_log,
+                ["L"] = nil,
+                ["<c-b>"] = actions.scroll_view(-0.25),
+                ["<c-f>"] = actions.scroll_view(0.25),
+                ["<tab>"] = actions.select_next_entry,
+                ["<s-tab>"] = actions.select_prev_entry,
+                ["gf"] = actions.goto_file,
+                ["<C-w><C-f>"] = actions.goto_file_split,
+                ["<C-w>gf"] = actions.goto_file_tab,
+                ["i"] = actions.listing_style,
+                ["f"] = actions.toggle_flatten_dirs,
+                ["<leader>e"] = actions.focus_files,
+                ["<leader>b"] = actions.toggle_files,
+                ["g<C-x>"] = actions.cycle_layout,
+                ["[x"] = actions.prev_conflict,
+                ["]x"] = actions.next_conflict,
             },
             file_history_panel = {
-                ["g!"] = cb "options",
-                ["<C-A-d>"] = cb "open_in_diffview",
-                ["y"] = cb "copy_hash",
-                ["zR"] = cb "open_all_folds",
-                ["zM"] = cb "close_all_folds",
-                ["j"] = cb "next_entry",
-                ["<down>"] = cb "next_entry",
-                ["k"] = cb "prev_entry",
-                ["<up>"] = cb "prev_entry",
-                ["<cr>"] = cb "select_entry",
-                ["o"] = cb "select_entry",
-                ["<2-LeftMouse>"] = cb "select_entry",
-                ["<tab>"] = cb "select_next_entry",
-                ["<s-tab>"] = cb "select_prev_entry",
-                ["gf"] = cb "goto_file",
-                ["<C-w><C-f>"] = cb "goto_file_split",
-                ["<C-w>gf"] = cb "goto_file_tab",
-                ["<leader>e"] = cb "focus_files",
-                ["<leader>b"] = cb "toggle_files",
+                ["g!"] = actions.options,
+                ["<C-A-d>"] = actions.open_in_diffview,
+                ["y"] = actions.copy_hash,
+                ["L"] = actions.open_commit_log,
+                ["zR"] = actions.open_all_folds,
+                ["zM"] = actions.close_all_folds,
+                ["j"] = actions.next_entry,
+                ["<down>"] = actions.next_entry,
+                ["k"] = actions.prev_entry,
+                ["<up>"] = actions.prev_entry,
+                ["<cr>"] = actions.select_entry,
+                ["o"] = actions.select_entry,
+                ["<2-LeftMouse>"] = actions.select_entry,
+                ["<c-b>"] = actions.scroll_view(-0.25),
+                ["<c-f>"] = actions.scroll_view(0.25),
+                ["<tab>"] = actions.select_next_entry,
+                ["<s-tab>"] = actions.select_prev_entry,
+                ["gf"] = actions.goto_file,
+                ["<C-w><C-f>"] = actions.goto_file_split,
+                ["<C-w>gf"] = actions.goto_file_tab,
+                ["<leader>e"] = actions.focus_files,
+                ["<leader>b"] = actions.toggle_files,
+                ["g<C-x>"] = actions.cycle_layout,
             },
             option_panel = {
-                ["<tab>"] = cb "select",
-                ["q"] = cb "close",
+                ["<tab>"] = actions.select_entry,
+                ["q"] = actions.close,
             },
         },
     }
@@ -105,7 +176,10 @@ return {
         "DiffviewToggleFiles",
         "DiffviewClose",
         "DiffviewRefresh",
+        "DiffviewToggle",
+        "DiffviewFileHistory",
     },
     config = config,
+    setup = setup,
     module = "diffview",
 }
