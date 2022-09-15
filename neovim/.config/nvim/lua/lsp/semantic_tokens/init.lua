a4fd99212141b9e47eb611756b5f4b09b23b6832
tiagovla <tiagovla@gmail.com>
2022-09-15 20:12:34 -0300

    chore: remove semantic tokens local plugin

--- neovim/.config/nvim/lua/lsp/semantic_tokens/init.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/neovim/neovim/pull/15723
-require "lsp.semantic_tokens.core.handlers"
-require "lsp.semantic_tokens.core.buf"
-require "lsp.semantic_tokens.core.protocol"
-
--- https://github.com/theHamsta/nvim-semantic-tokens
-require("lsp.semantic_tokens.plugin.nvim_semantic_tokens").setup()
