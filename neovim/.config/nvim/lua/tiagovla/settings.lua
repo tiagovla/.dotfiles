64dffd6ef90d865c83afdba74d262c096d74962a
tiagovla <tiagovla@gmail.com>
2022-12-24 01:20:52 -0300

    refactor: remove aliases and unused functions

--- neovim/.config/nvim/lua/tiagovla/settings.lua
+++ neovim/.config/nvim/lua/tiagovla/settings.lua
@@ -1,26 +1,21 @@
 local M = {}
 
-local g, opt, cmd = vim.g, vim.opt, vim.cmd
-
 function M.setup()
-    M.options()
     M.globals()
-    M.window_options()
-    M.buffer_options()
+    M.options()
     M.commands()
-    M.builtins()
 end
 
 function M.globals()
-    g.mapleader = " "
-    g.python3_host_prog = "$HOME/.asdf/shims/python"
-    g.format_on_save = true
-    g.tex_conceal = "abdgms"
-    g.tex_flavor = "latex"
+    vim.g.mapleader = " "
+    vim.g.python3_host_prog = "$HOME/.asdf/shims/python"
+    vim.g.format_on_save = true
+    vim.g.tex_conceal = "abdgms"
+    vim.g.tex_flavor = "latex"
 end
 
 function M.options()
-    opt.sessionoptions = {
+    vim.opt.sessionoptions = {
         "blank",
         "buffers",
         "curdir",
@@ -33,122 +28,93 @@ function M.options()
         "winpos",
         "terminal",
     }
-    opt.clipboard = "unnamedplus"
-    opt.backupdir:remove { "." }
+    vim.opt.clipboard = "unnamedplus"
+    vim.opt.backupdir:remove { "." }
 
-    opt.backup = true
-    opt.backupcopy = "yes"
-    opt.cmdheight = 1
-    opt.completeopt = { "menuone", "noinsert", "noselect" }
+    vim.opt.backup = true
+    vim.opt.backupcopy = "yes"
+    vim.opt.cmdheight = 1
+    vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
 
-    opt.history = 1000
-    opt.joinspaces = false
+    vim.opt.history = 1000
+    vim.opt.joinspaces = false
 
-    opt.incsearch = true
-    opt.infercase = true
-    opt.inccommand = "split"
+    vim.opt.incsearch = true
+    vim.opt.infercase = true
+    vim.opt.inccommand = "split"
 
-    opt.scrolloff = 10
-    opt.sidescrolloff = 10
+    vim.opt.scrolloff = 10
+    vim.opt.sidescrolloff = 10
 
-    opt.showbreak = "↳"
-    opt.splitkeep = "topline"
-    opt.smarttab = true
-    -- opt.wildmode = { list = "longest" }
+    vim.opt.showbreak = "↳"
+    vim.opt.splitkeep = "topline"
+    vim.opt.smarttab = true
+    -- vim.opt.wildmode = { list = "longest" }
 
-    opt.termguicolors = true
+    vim.opt.termguicolors = true
 
-    opt.shiftwidth = 4
-    opt.tabstop = 4
-    opt.softtabstop = 0
-    opt.expandtab = true
-    opt.smartindent = true
-    opt.shiftwidth = 4
-    opt.expandtab = true
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 0
+    vim.opt.expandtab = true
+    vim.opt.smartindent = true
+    vim.opt.shiftwidth = 4
+    vim.opt.expandtab = true
 
-    opt.fillchars = { eob = " " }
+    vim.opt.fillchars = { eob = " " }
 
-    opt.hidden = true
-    opt.ignorecase = true
-    opt.smartcase = true
-    opt.mouse = "a"
+    vim.opt.hidden = true
+    vim.opt.ignorecase = true
+    vim.opt.smartcase = true
+    vim.opt.mouse = "a"
 
-    opt.shortmess:append "cAIfs"
-    opt.signcolumn = "yes"
-    opt.splitbelow = true
-    opt.splitright = true
+    vim.opt.shortmess:append "cAIfs"
+    vim.opt.signcolumn = "yes"
+    vim.opt.splitbelow = true
+    vim.opt.splitright = true
 
-    opt.timeoutlen = 1200
-    opt.spelloptions:append "noplainbuffer"
+    vim.opt.timeoutlen = 1200
+    vim.opt.spelloptions:append "noplainbuffer"
 
-    opt.showmode = false
-    opt.hlsearch = true
-    opt.synmaxcol = 150
-    opt.cursorline = true
-    opt.updatetime = 250
-    opt.whichwrap:append "<>[]hl"
+    vim.opt.showmode = false
+    vim.opt.hlsearch = true
+    vim.opt.synmaxcol = 150
+    vim.opt.cursorline = true
+    vim.opt.updatetime = 250
+    vim.opt.whichwrap:append "<>[]hl"
 
-    opt.undofile = true
+    vim.opt.undofile = true
 
-    opt.expandtab = true
-    opt.smartindent = true
-    opt.shiftwidth = 4
-    opt.tabstop = 8
+    vim.opt.expandtab = true
+    vim.opt.smartindent = true
+    vim.opt.shiftwidth = 4
+    vim.opt.tabstop = 8
 
-    opt.number = true
-    opt.numberwidth = 2
-    opt.relativenumber = true
-    opt.ruler = false
+    vim.opt.number = true
+    vim.opt.numberwidth = 2
+    vim.opt.relativenumber = true
+    vim.opt.ruler = false
 
-    opt.wrap = false
+    vim.opt.wrap = false
 
-    opt.listchars = {
+    vim.opt.listchars = {
         tab = "!·",
         nbsp = "␣",
         trail = "·",
         eol = "↲",
     }
-    opt.list = true
-    opt.colorcolumn = tostring(80)
-    opt.conceallevel = 1
-end
-
-function M.window_options() end
-
-function M.buffer_options() end
-
-function M.builtins()
-    g.loaded_gzip = 1
-    g.loaded_zip = 1
-    g.loaded_zipPlugin = 1
-    g.loaded_tar = 1
-    g.loaded_tarPlugin = 1
-
-    g.loaded_getscript = 1
-    g.loaded_getscriptPlugin = 1
-    g.loaded_vimball = 1
-    g.loaded_vimballPlugin = 1
-    g.loaded_2html_plugin = 1
-
-    g.loaded_matchit = 1
-    g.loaded_matchparen = 1
-    g.loaded_logiPat = 1
-    g.loaded_rrhelper = 1
-
-    g.loaded_netrw = 1
-    g.loaded_netrwPlugin = 1
-    g.loaded_netrwSettings = 1
-    g.loaded_netrwFileHandlers = 1
-    g.did_load_filetypes = 1
+    vim.opt.list = true
+    vim.opt.colorcolumn = tostring(80)
+    vim.opt.conceallevel = 1
 end
 
 function M.commands()
-    cmd "set fsync"
-    cmd "set t_ZH=^[[3m"
-    cmd "set t_ZR=^[[23m"
-    cmd [[ :cab W w]]
-    cmd [[ :cab Q q]]
-    cmd [[ autocmd BufRead * autocmd FileType <buffer> ++once
+    vim.cmd "set fsync"
+    vim.cmd "set t_ZH=^[[3m"
+    vim.cmd "set t_ZR=^[[23m"
+    vim.cmd [[ :cab W w]]
+    vim.cmd [[ :cab Q q]]
+    vim.cmd [[ autocmd BufRead * autocmd FileType <buffer> ++once
       \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
 end
 
