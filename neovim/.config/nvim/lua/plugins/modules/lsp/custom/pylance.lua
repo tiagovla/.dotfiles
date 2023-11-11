0e5f285d305a965ca6441df84fb9d1429d5da809
tiagovla <tiagovla@gmail.com>
2023-11-10 21:01:45 -0500

    fix: custom python lsp server

--- neovim/.config/nvim/lua/plugins/modules/lsp/custom/pylance.lua
+++ neovim/.config/nvim/lua/plugins/modules/lsp/custom/pylance.lua
@@ -128,7 +128,7 @@ end
 local function installer(ctx)
     local script = [[
     curl -s -c cookies.txt 'https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance' > /dev/null &&
-    curl -s "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/latest/vspackage"
+    curl -s "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/2023.11.10/vspackage"
          -j -b cookies.txt --compressed --output "pylance.vsix"
     ]]
     ctx.receipt:with_primary_source(ctx.receipt.unmanaged)
