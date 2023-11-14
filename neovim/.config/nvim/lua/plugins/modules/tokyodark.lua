b04637de6f0bb70e72ed35f44ba85cacdcbd6535
tiagovla <tiagovla@gmail.com>
2023-11-14 02:10:48 -0500

    chore: update theme settings

--- neovim/.config/nvim/lua/plugins/modules/tokyodark.lua
+++ neovim/.config/nvim/lua/plugins/modules/tokyodark.lua
@@ -3,34 +3,34 @@ return {
     lazy = false,
     dev = true,
     priority = 1000,
-    config = function()
-        vim.g.tokyodark_transparent_background = true
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            callback = function()
-                local function hl(...)
-                    vim.api.nvim_set_hl(0, ...)
-                end
-                hl("LspInlayHint", { bg = "#1C1C2A", fg = "#9AA0A7" })
-                hl("@module", { link = "TSType" })
-                hl("@property", { link = "Identifier" })
-                hl("@variable", { fg = "#Afa8ea" })
-                hl("FloatTitle", { link = "Blue" })
-                hl("TelescopeBorder", { link = "TSType" })
-                hl("TelescopePreviewBorder", { fg = "#4A5057" })
-                hl("TelescopePreviewTitle", { link = "Blue" })
-                hl("TelescopePromptBorder", { fg = "#4A5057" })
-                hl("@lsp.type.variable", {})
-                hl("TelescopePromptTitle", { link = "Blue" })
-                hl("TelescopeResultsBorder", { fg = "#4A5057" })
-                hl("TelescopeResultsTitle", { link = "Blue" })
-                hl("CmpItemKindCopilot", { fg = "#6CC644" })
-                hl("NoiceLspProgressSpinner", { bg = "#1C1C2A" })
-                hl("NoiceLspProgressClient", { bg = "#1C1C2A" })
-                hl("NoiceLspProgressTitle", { bg = "#1C1C2A" })
-                hl("NoiceMini", { bg = "#1C1C2A" })
-                hl("NoiceCmdlineIconSearch", { link = "Blue" })
-            end,
-        })
-        vim.cmd.colorscheme "tokyodark"
+    opts = {
+        transparent_background = true,
+        custom_highlights = function(hl, p)
+            return {
+                ["LspInlayHint"] = { bg = "#1C1C2A", fg = "#9AA0A7" },
+                ["@module"] = { link = "TSType" },
+                ["@property"] = { link = "Identifier" },
+                ["@variable"] = { fg = "#Afa8ea" },
+                ["FloatTitle"] = { link = "Blue" },
+                ["TelescopeBorder"] = { link = "TSType" },
+                ["TelescopePreviewBorder"] = { fg = "#4A5057" },
+                ["TelescopePreviewTitle"] = { link = "Blue" },
+                ["TelescopePromptBorder"] = { fg = "#4A5057" },
+                ["@lsp.type.variable"] = {},
+                ["TelescopePromptTitle"] = { link = "Blue" },
+                ["TelescopeResultsBorder"] = { fg = "#4A5057" },
+                ["TelescopeResultsTitle"] = { link = "Blue" },
+                ["CmpItemKindCopilot"] = { fg = "#6CC644" },
+                ["NoiceLspProgressSpinner"] = { bg = "#1C1C2A" },
+                ["NoiceLspProgressClient"] = { bg = "#1C1C2A" },
+                ["NoiceLspProgressTitle"] = { bg = "#1C1C2A" },
+                ["NoiceMini"] = { bg = "#1C1C2A" },
+                ["NoiceCmdlineIconSearch"] = { link = "Blue" },
+            }
+        end,
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts)
+        require("tokyodark").colorscheme()
     end,
 }
