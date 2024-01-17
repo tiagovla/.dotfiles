ac73493902b41f93d8b664fa76993f88e78cfcc1
tiagovla <tiagovla@gmail.com>
2024-01-16 22:08:00 -0500

    chore: update bufferline with harpoon feat

--- neovim/.config/nvim/lua/plugins/modules/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/modules/bufferline.lua
@@ -1,11 +1,18 @@
 return {
     "akinsho/nvim-bufferline.lua",
     lazy = false,
+    init = function()
+        vim.keymap.set("n", "<c-n>", "<cmd>BufferLineCycleNext<cr>")
+        vim.keymap.set("n", "<c-p>", "<cmd>BufferLineCyclePrev<cr>")
+    end,
     opts = {
         options = {
             view = "multiwindow",
             numbers = function(opts)
-                return string.format("%s", opts.ordinal)
+                local harpoon = require "harpoon.mark"
+                local buf_name = vim.api.nvim_buf_get_name(opts.id)
+                local harpoon_mark = harpoon.get_index_of(buf_name)
+                return harpoon_mark
             end,
             modified_icon = "●",
             max_prefix_length = 5,
@@ -19,6 +26,18 @@ return {
                     text_align = "center",
                 },
             },
+            sort_by = function(buffer_a, buffer_b)
+                local function get_local_mark(id)
+                    local harpoon = require "harpoon.mark"
+                    local buf_name = vim.api.nvim_buf_get_name(id)
+                    local harpoon_mark = harpoon.get_index_of(buf_name)
+                    if harpoon_mark == nil then
+                        return 999999
+                    end
+                    return harpoon_mark
+                end
+                return get_local_mark(buffer_a.id) < get_local_mark(buffer_b.id)
+            end,
         },
     },
 }
