30bb8e7d1203ba7379f4c835b1ae2cc0ad21545e
tiagovla <tiagovla@gmail.com>
2025-05-19 20:43:19 -0300

    chore: upgrade to harpoon2

--- neovim/.config/nvim/lua/plugins/modules/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/modules/bufferline.lua
@@ -1,3 +1,15 @@
+local function get_harpoon_mark_from_id(id)
+    local harpoon = require "harpoon"
+    local Path = require "plenary.path"
+    local list = harpoon:list()
+
+    local buf_name = vim.api.nvim_buf_get_name(id)
+    local root_dir = list.config.get_root_dir()
+    local n_path = Path:new(buf_name):make_relative(root_dir)
+    local _, idx = list:get_by_value(n_path)
+    return idx
+end
+
 return {
     "akinsho/nvim-bufferline.lua",
     lazy = false,
@@ -9,10 +21,7 @@ return {
         options = {
             view = "multiwindow",
             numbers = function(opts)
-                local harpoon = require "harpoon.mark"
-                local buf_name = vim.api.nvim_buf_get_name(opts.id)
-                local harpoon_mark = harpoon.get_index_of(buf_name)
-                return harpoon_mark
+                return get_harpoon_mark_from_id(opts.id)
             end,
             modified_icon = "●",
             max_prefix_length = 5,
@@ -28,13 +37,7 @@ return {
             },
             sort_by = function(buffer_a, buffer_b)
                 local function get_local_mark(id)
-                    local harpoon = require "harpoon.mark"
-                    local buf_name = vim.api.nvim_buf_get_name(id)
-                    local harpoon_mark = harpoon.get_index_of(buf_name)
-                    if harpoon_mark == nil then
-                        return 999999
-                    end
-                    return harpoon_mark
+                    return get_harpoon_mark_from_id(id) or 999999
                 end
                 return get_local_mark(buffer_a.id) < get_local_mark(buffer_b.id)
             end,
--- neovim/.config/nvim/lua/plugins/modules/harpoon.lua
+++ neovim/.config/nvim/lua/plugins/modules/harpoon.lua
@@ -1,19 +1,35 @@
 return {
     "ThePrimeagen/harpoon",
     lazy = false,
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local opts = { noremap = true, silent = true }
+        local harpoon = require "harpoon"
+        harpoon:setup()
+        vim.keymap.set("n", "<leader><Tab>", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, opts)
+
         for i = 1, 9 do
             vim.keymap.set("n", "<leader>" .. i, function()
-                require("harpoon.ui").nav_file(i)
+                harpoon:list():select(i)
             end, { desc = "Harpoon " .. i .. " file" })
         end
         vim.keymap.set("n", "<s-m>", function()
-            require("harpoon.mark").add_file()
-            vim.notify "󱡅  marked file"
+            harpoon:list():add()
+            vim.notify "marked file"
         end, opts)
-        vim.keymap.set("n", "<leader><Tab>", function()
-            require("harpoon.ui").toggle_quick_menu()
-        end, opts)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "harpoon",
+            callback = function()
+                for i = 1, 9 do
+                    vim.keymap.set("n", tostring(i), function()
+                        harpoon:list():select(i)
+                    end, { buffer = true })
+                end
+            end,
+        })
     end,
 }
