68a920aff604030ea2b83d145dfcd04412f1650f
tiagovla <tiagovla@gmail.com>
2025-07-28 01:20:33 -0300

    refactor: picom settings

--- picom/.config/picom/launch.sh
+++ picom/.config/picom/launch.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q picom
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
-
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/picom.log
 picom --backend glx 2>&1 | tee -a /tmp/picom.log &
 disown
-
 echo "Picom launched..."
