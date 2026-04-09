698e7fb8a21c95cf44e75cbbfd89787df920ab04
tiagovla <tiagovla@gmail.com>
2026-04-08 22:25:05 -0300

    perf: remove unused dependency

--- neovim/.config/nvim/plugin/neogen.lua
+++ neovim/.config/nvim/plugin/neogen.lua
@@ -1,7 +1,4 @@
-vim.pack.add({
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/danymat/neogen",
-}, { confirm = false })
+vim.pack.add({ "https://github.com/danymat/neogen" }, { confirm = false })
 
 require("neogen").setup {
     enabled = true,
