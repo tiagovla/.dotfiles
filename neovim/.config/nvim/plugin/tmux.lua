48662fe2037add8bdede8a178e5143a3af55de1e
tiagovla <tiagovla@gmail.com>
2026-04-08 21:36:29 -0300

    fix: do not sync clipboard

--- neovim/.config/nvim/plugin/tmux.lua
+++ neovim/.config/nvim/plugin/tmux.lua
@@ -2,7 +2,7 @@ vim.pack.add({ "https://github.com/aserowy/tmux.nvim" }, { confirm = false })
 
 require("tmux").setup {
     copy_sync = {
-        enable = true,
+        enable = false,
     },
     navigation = {
         enable_default_keybindings = true,
